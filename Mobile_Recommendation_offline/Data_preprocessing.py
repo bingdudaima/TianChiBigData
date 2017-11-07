d156c4f0468b88c75c3f5994e9e9387866db33d9
hadoop <594632557@qq.com>
2017-11-07 23:05:00 +0800

    all

--- Mobile_Recommendation_offline/Data_preprocessing.py
+++ Mobile_Recommendation_offline/Data_preprocessing.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    userTable=pd.read_csv('../DataSet/tianchi_fresh_comp_train_user.csv',\
-    usecols = ['user_id','item_id','behavior_type','time'])
-    itemTable=pd.read_csv('../DataSet/tianchi_fresh_comp_train_item.csv')
-    userTable=userTable[userTable.item_id.isin(list(itemTable.item_id))]
-    userTable['days']=userTable['time'].map(lambda x:x.split(' ')[0])
-    userTable['hour']=userTable['time'].map(lambda x:x.split(' ')[1])
-    userTable=userTable[userTable['days'] != '2014-12-11']
-    userTable=userTable[userTable['days'] != '2014-12-12']
+    userTable=pd.read_csv('../DataSet/tianchi_fresh_comp_train_user.csv')
+    itemTable = pd.read_csv('../DataSet/tianchi_fresh_comp_train_item.csv')
+    userTable = userTable[userTable.item_id.isin(list(itemTable.item_id))]
+    userTable['days'] = userTable['time'].map(lambda x:x.split(' ')[0])
+    userTable['hour'] = userTable['time'].map(lambda x:x.split(' ')[1])
+    userTable = userTable[userTable['days'] != '2014-12-11']
+    userTable = userTable[userTable['days'] != '2014-12-12']
     userTable.to_csv('../DataSet/subData.csv',index=None)
